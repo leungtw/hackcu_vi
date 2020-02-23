e389b6b4abe726fac12875af632fb7995f96a5ff
sale3054 <sale3054@colorado.edu>
2020-02-22 20:43:14 -0700

    added changes to app.py

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, render_template, redirect
 from flask import flash, request
 from flask import jsonify
+from pymongo import MongoClient
+from pprint import pprint
+
 import json
-app = Flask(__name__)
+
+#====== helper methods ======
+def setup_mongo_client():
+	usr_name = "sale3054"
+	pw 	     = "examplepw123"
+	client = MongoClient('''mongodb://'''+usr_name+''':'''+ pw +
+						 '''@cluster0-shard-00-00-g3da1.mongodb.net:27017,cluster0-shard-00-01-g3da1.mongodb.net:27017,cluster0-shard-00-02-g3da1.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority''')
+	return client
+#============================
 
 @app.route('/')
 def index_page():
@@ -16,23 +27,26 @@ def home_page():
 def data_page():
 	return render_template('data.html')
 
-@app.route("/hello")
-def hello_page():
-	return "This is the hello world page."
-
 @app.route("/submit", methods=['GET', 'POST'])
 def submit():
-	company = request.form["company"]	
-	position = request.form["position"]
-	location = request.form["location"]
-	link = request.form["link"]
-	try:
-		submitted = request.form["submitted"]
-	except:
-		submitted = "False"
 
-	print(company, position, location, link, submitted)
-	return render_template("data.html")
+	response = dict(request.form)
+
+	del response["submit"]
+	if "submitted" not in response:
+		response["submitted"] = "false"
+	pprint(response)
+
+
+	posts = db.posts
+	post_id = posts.insert_one(response)
+	return redirect("data")
 
 def page_not_found(e):
 	return "You suck", e
+
+if __name__ == "__main__":
+	app = Flask(__name__)
+
+	client = setup_mongo_client()
+	db = client.job_db
